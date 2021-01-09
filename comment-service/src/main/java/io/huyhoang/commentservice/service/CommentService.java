8429cbfbac21eb70822bcf85de0f34f386221fdf
Hoang Le <kuhpro@gmail.com>
2021-01-08 23:51:26 -0600

    connect to postgresql database

--- comment-service/src/main/java/io/huyhoang/commentservice/service/CommentService.java
+++ comment-service/src/main/java/io/huyhoang/commentservice/service/CommentService.java
@@ -1,4 +1,7 @@
 package io.huyhoang.commentservice.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CommentService {
 }
--- comment-service/src/main/resources/schema.sql
+++ comment-service/src/main/resources/schema.sql
@@ -1,6 +1,11 @@
-CREATE TABLE comments (comment_id UUID DEFAULT RANDOM_UUID() PRIMARY KEY,
-content VARCHAR(255),
- user_id UUID,
-  post_id UUID,
-   created_at TIMESTAMP,
-    updated_at TIMESTAMP);
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS comments (
+    comment_id UUID DEFAULT uuid_generate_v4(),
+    content VARCHAR(255),
+    user_id UUID,
+    post_id UUID,
+    created_at TIMESTAMP ,
+    updated_at TIMESTAMP,
+    PRIMARY KEY (comment_id)
+);
