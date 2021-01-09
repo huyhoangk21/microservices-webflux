0e5aeebc5e663f46e5ca296f24a573ea1df34aff
Hoang Le <kuhpro@gmail.com>
2021-01-09 15:00:06 -0600

    add placeholder user service, profile service, following service, follower service

---
+++ follower-service/src/main/java/io/huyhoang/followerservice/FollowerServiceApplication.java
@@ -0,0 +1,13 @@
+package io.huyhoang.followerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FollowerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FollowerServiceApplication.class, args);
+    }
+
+}
